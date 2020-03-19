30522ac455793633f7aeec890fe53de7f71cbc61
Kael Zhang <i+github@kael.me>
2020-03-19 20:28:42 +0800

    reducer: main

--- compton/reducer.py
+++ compton/reducer.py
@@ -1,13 +1,17 @@
 from abc import ABC, abstractmethod
-# from typing import (
-#     Optional,
-#     Tuple,
-# )
+from typing import (
+    Optional,
+    Tuple,
+)
 
 from .types import (
     Vector,
     Symbol,
     Payload
+)
+
+from .common import (
+    get_hierachical
 )
 
 
@@ -22,17 +26,57 @@ class Reducer(ABC):
     @abstractmethod
     def vector(self) -> Vector:
         """The vector of a reducer could be a more generic vector which is much
-        shorter
+        shorter.
+
+        Reducer::vector always does semi matching
         """
 
         return
 
-    @abstractmethod
     def reduce(
         self,
         previous: Payload,
         payload: Payload,
         symbol: Symbol,
         vector: Vector
-    ):
+    ) -> Tuple[bool, Optional[Payload]]:
+        """Applies the update payload
+
+        Args:
+            previous (Payload):
+
+        Returns:
+            Tuple[bool, Optional[Payload]]:
+            - the first item in the tuple indicates whether the data changes.
+            - If no changes, the second item will be `None`
+        """
+
+        last = vector[-1]
+        partial_vector = (symbol, vector[:-1])
+
+        parent = get_hierachical(
+            self._not_updated,
+            partial_vector,
+            {},
+            True
+        )
+
+        not_updated = parent.get(last, None)
+
+        if not previous:
+            parent[last] = self.merge(
+                not_updated,
+                payload
+            ) if not_updated else payload
+
+            return False, None
+
+        return True, self.merge(previous, not_updated)
+
+    @abstractmethod
+    def merge(
+        self,
+        target: Payload,
+        payload: Payload
+    ) -> Payload:
         pass
